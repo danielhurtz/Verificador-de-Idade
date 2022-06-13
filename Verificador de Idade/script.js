66f7d917ddb84062ce54bd439a85c1b5bc99fa47
danielhurtz <daniel.anjos@protonmail.com>
2022-06-13 17:00:04 -0300

    Atualizacao pasta img

--- Verificador de Idade/script.js
+++ Verificador de Idade/script.js
@@ -17,50 +17,50 @@ function verificar() {
             genero = 'Homem'
             if (idade >= 0 && idade < 5) {
                 // Bebe 
-                img.setAttribute('src', 'H1.png')
+                img.setAttribute('src', './img/H1.png')
             } else if (idade < 12) {
                 // Crianca
-                img.setAttribute('src', 'H2.png')
+                img.setAttribute('src', './img/H2.png')
             } else if (idade < 19) {
                 // Adolescente
-                img.setAttribute('src', 'H3.png')
+                img.setAttribute('src', './img/H3.png')
             } else if (idade < 30) {
                 // Adulto
-                img.setAttribute('src', 'H4.png')
+                img.setAttribute('src', './img/H4.png')
             } else if (idade < 40) {
                 // Adulto + velho
-                img.setAttribute('src', 'H5.png')
+                img.setAttribute('src', './img/H5.png')
             } else if (idade < 60) {
                 // Adulto ++velho
-                img.setAttribute('src', 'H6.png')
+                img.setAttribute('src', './img/H6.png')
             } else {
                 // Idoso
-                img.setAttribute('src', 'H7.png')
+                img.setAttribute('src', './img/H7.png')
             }
 
         } else if (sexF[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 5) {
                 // Bebe 
-                img.setAttribute('src', 'M1.png')
+                img.setAttribute('src', './img/M1.png')
             } else if (idade < 12) {
                 // Crianca
-                img.setAttribute('src', 'M2.png')
+                img.setAttribute('src', './img/M2.png')
             } else if (idade < 19) {
                 // Adolescente
-                img.setAttribute('src', 'M3.png')
+                img.setAttribute('src', './img/M3.png')
             } else if (idade < 30) {
                 // Adulta
-                img.setAttribute('src', 'M4.png')
+                img.setAttribute('src', './img/M4.png')
             } else if (idade < 40) {
                 // Adulta + velha
-                img.setAttribute('src', 'M5.png')
+                img.setAttribute('src', './img/M5.png')
             } else if (idade < 60) {
                 // Adulta ++velha
-                img.setAttribute('src', 'M6.png')
+                img.setAttribute('src', './img/M6.png')
             } else {
                 // Idosa
-                img.setAttribute('src', 'M7.png')
+                img.setAttribute('src', './img/M7.png')
             }
         }
 
